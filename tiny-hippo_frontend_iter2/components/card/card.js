553c8ca13f63043e25911c816df01cba46d129c4
Ecr23 <370095872@qq.com>
2018-05-17 10:06:07 +0800

    update iter2

---
+++ tiny-hippo_frontend_iter2/components/card/card.js
@@ -0,0 +1,29 @@
+// components/card/card.js
+Component({
+
+  properties: {
+    title: {
+      type: String
+    },
+    description: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    dishId: {
+      type: Number
+    }
+
+  },
+
+  data: {
+
+  },
+
+  methods: {
+    setBackgroundImage: function() {
+      
+    }
+  }
+})
