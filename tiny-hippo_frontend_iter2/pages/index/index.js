a24c0a2305acbd8fed0dfc119ef095b62da56e7b
Ecr23 <370095872@qq.com>
2018-05-23 09:50:02 +0800

    remove conflicts

--- tiny-hippo_frontend_iter2/pages/index/index.js
+++ tiny-hippo_frontend_iter2/pages/index/index.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    imgUrls: [
-      '../../images/1.jpg',
-      '../../images/2.jpg',
-      '../../images/3.jpg',
-      '../../images/4.jpg',
-      '../../images/5.jpg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 2500,
-    duration: 1000,
-    listData: [],
-=======
 const hSwiper = require('../../component/hSwiper/hSwiper.js');
 Page({
 	data: {
@@ -26,7 +6,6 @@ Page({
     menuSwiperVar: {},
     listData: [],
     foodTypes: 0,
->>>>>>> features/BeAShaper
     activeIndex: 0,
     toView: 'a1',
     scrollTop: 300,
@@ -37,19 +16,6 @@ Page({
     cartList: [],
     tolMoney: 0,
     foodNumber: 0,
-<<<<<<< HEAD
-    showCart: false,
-    loading: false,
-  },
-
-  onLoad: function (options) {
-    var that = this;
-    var sysinfo = wx.getSystemInfoSync().windowHeight;
-    console.log(sysinfo)
-    wx.request({
-      /* 自己写的json数据的网址 */
-      url: 'https://easy-mock.com/mock/5ab2750509c2ed0d826ee129/example/hippo',
-=======
     loading: true,
     foodTypeList : {},
     styleValue:{},
@@ -72,7 +38,6 @@ Page({
     wx.request({
       /* 自己写的json数据的网址 */
       url: 'https://www.easy-mock.com/mock/5ab2750509c2ed0d826ee129/example/hippo',
->>>>>>> features/BeAShaper
       method: 'GET',
       data: {},
       header: {
@@ -80,26 +45,6 @@ Page({
       },
       success: function (res) {
         wx.hideLoading();
-<<<<<<< HEAD
-        console.log(res)
-        that.setData({
-          listData: res.data,
-          loading: true
-        })
-      }
-    })
-    
-  },
-  /**
-   * 选择左侧菜单
-   */
-  selectMenu: function (e) {
-    var index = e.currentTarget.dataset.index;
-    console.log(index)
-    this.setData({
-      activeIndex: index,
-      toView: 'a' + index
-=======
         // console.log(res)
         that.setData({
           listData: res.data,
@@ -331,7 +276,6 @@ Page({
     animation.translateX(x).translate3d(0).step();
     this.setData({
       ["swiperAnmiation." + id]: animation.export()
->>>>>>> features/BeAShaper
     })
   },
   /**
@@ -343,23 +287,6 @@ Page({
     var content_height = 73;
     var listData = this.data.listData;
     var count = listData.length;
-<<<<<<< HEAD
-    
-    var up = content_height * (listData[0].foods.length-1) + tab_height;
-    var down = 0;
-    var index = count-1;
-    for (var i = 1; i < count; i++) {
-          if (dis >= down && dis < up) {
-            index = i-1;
-            console.log(index);
-            break;
-          } else {
-            down = up;
-            up += content_height * (listData[i].foods.length) + tab_height;
-          }
-    }
-    if (index >= count/2) {
-=======
 
     var up = content_height * (listData[0].foods.length - 1) + tab_height;
     var down = 0;
@@ -375,7 +302,6 @@ Page({
       }
     }
     if (index >= count / 2) {
->>>>>>> features/BeAShaper
       wx.pageScrollTo({
         scrollTop: 100
       })
@@ -388,23 +314,14 @@ Page({
    * 添加到购物车
    */
   addToCart: function (e) {
-<<<<<<< HEAD
-=======
     // console.log(e)
->>>>>>> features/BeAShaper
     var type = e.currentTarget.dataset.type;
     var index = e.currentTarget.dataset.index;
     var that = this;
     var selected_item = this.data.listData[type].foods[index];
-<<<<<<< HEAD
-    console.log(selected_item.number);
-    var listData = this.data.listData;
-    var number = selected_item.number+1;
-=======
     // console.log(selected_item.number);
     var listData = this.data.listData;
     var number = selected_item.number + 1;
->>>>>>> features/BeAShaper
     for (var i = 0; i < listData.length; i++) {
       for (var j = 0; j < listData[i].foods.length; j++) {
         if (listData[i].foods[j].name == selected_item.name) {
@@ -418,11 +335,6 @@ Page({
       currentIndex: index,
       listData: listData
     });
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> features/BeAShaper
     var a = this.data;
     var unselected = true;
     var cartList = this.data.cartList;
@@ -442,17 +354,11 @@ Page({
         "price": a.listData[a.currentType].foods[a.currentIndex].price,
         "number": number,
         "sum": a.listData[a.currentType].foods[a.currentIndex].price,
-<<<<<<< HEAD
-      }
-      sum = a.tolMoney + a.listData[a.currentType].foods[a.currentIndex].price;
-      
-=======
         "type": type,
         "index": index
       }
       sum = a.tolMoney + a.listData[a.currentType].foods[a.currentIndex].price;
 
->>>>>>> features/BeAShaper
       cartList.push(addItem);
     }
     this.setData({
@@ -462,11 +368,7 @@ Page({
     });
   },
   /*从图片处减少商品*/
-<<<<<<< HEAD
-  minusFromMenu: function(e) {
-=======
   minusFromMenu: function (e) {
->>>>>>> features/BeAShaper
     var type = e.currentTarget.dataset.type;
     var index = e.currentTarget.dataset.index;
     var that = this;
@@ -485,15 +387,9 @@ Page({
     var sum;
     for (var i = 0; i < cartList.length; i++) {
       if (cartList[i].name == this.data.listData[type].foods[index].name) {
-<<<<<<< HEAD
-          sum = this.data.tolMoney - cartList[i].price;
-          cartList[i].sum -= cartList[i].price;
-          cartList[i].number == 1 ? cartList.splice(i, 1) : cartList[i].number--;
-=======
         sum = this.data.tolMoney - cartList[i].price;
         cartList[i].sum -= cartList[i].price;
         cartList[i].number == 1 ? cartList.splice(i, 1) : cartList[i].number--;
->>>>>>> features/BeAShaper
       }
     }
     this.setData({
@@ -503,131 +399,11 @@ Page({
       foodNumber: this.data.foodNumber - 1
     });
   },
-<<<<<<< HEAD
-  /**
-   * 显示购物车
-   */
-  showCartList: function () {
-    if (this.data.cartList.length != 0) {
-      this.setData({
-        showCart: !this.data.showCart,
-      });
-    }
-  },
-  /**
-   * 清空购物车
-   */
-  clearCartList: function () {
-    var listData = this.data.listData;
-    for (var i = 0; i < listData.length; i++) {
-      for (var j = 0; j < listData[i].foods.length; j++) {
-          listData[i].foods[j].number = 0;
-      }
-    }
-    this.setData({
-      listData:listData,
-      cartList: [],
-      showCart: false,
-      tolMoney: 0,
-      foodNumber:0
-    });
-  },
-  /**
-   * 购物车中数量增加
-   */
-  addNumber: function (e) {
-    var type = e.currentTarget.dataset.type;
-    var index = e.currentTarget.dataset.index;
-    var that = this;
-    var cartList = this.data.cartList;
-    var listData = this.data.listData;
-    for (var i = 0; i < listData.length; i++) {
-      for (var j = 0; j < listData[i].foods.length; j++) {
-        if (cartList[index].name == listData[i].foods[j].name) {
-          listData[i].foods[j].number++;
-        }
-      }
-    }
-    console.log(index)
-    cartList[index].number++;
-    var sum = this.data.tolMoney + cartList[index].price;
-    cartList[index].sum += cartList[index].price;
-
-    this.setData({
-      listData:listData,
-      cartList: cartList,
-      tolMoney: sum,
-      foodNumber: this.data.foodNumber + 1
-    });
-  },
-  /**
-   * 购物车中数量减少
-   */
-  decNumber: function (e) {
-    var index = e.currentTarget.dataset.index;
-    console.log(index)
-    var cartList = this.data.cartList;
-    var listData = this.data.listData;
-    for (var i = 0; i < listData.length; i++) {
-      for (var j = 0; j < listData[i].foods.length; j++) {
-        if (cartList[index].name == listData[i].foods[j].name) {
-          listData[i].foods[j].number--;
-        }
-      }
-    }
-    var sum = this.data.tolMoney - cartList[index].price;
-    cartList[index].sum -= cartList[index].price;
-    cartList[index].number == 1 ? cartList.splice(index, 1) : cartList[index].number--;
-    this.setData({
-      listData:listData,
-      cartList: cartList,
-      tolMoney: sum,
-      showCart: cartList.length == 0 ? false : true,
-      foodNumber: this.data.foodNumber - 1
-    });
-  },
-  /**
-   * 点击”选好了“跳转到结算页面
-   */
-  checkout: function () {
-    if (this.data.sumMonney != 0) {
-      wx.setStorageSync('cartList', this.data.cartList);
-      wx.setStorageSync('tolMoney', this.data.tolMoney);
-      wx.setStorageSync('foodNumber', this.data.foodNumber);
-      wx.navigateTo({
-        url: '../../pages/payment/payment'   // 结算页面地址
-      })
-    }
-  },
-
-  onReady: function () {
-
-  },
-
-  onShow: function () {
-
-  },
-  onHide: function () {
-
-  },
-  onUnload: function () {
-
-  },
-  onPullDownRefresh: function () {
-
-  },
-  onReachBottom: function () {
-
-  },
-  onShareAppMessage: function () {
-
-=======
   onHide: function () {
     if (this.data.sumMonney != 0) {
       wx.setStorageSync('cartList', this.data.cartList);
       wx.setStorageSync('tolMoney', this.data.tolMoney);
       wx.setStorageSync('foodNumber', this.data.foodNumber)
     }
->>>>>>> features/BeAShaper
   }
 })
--- tiny-hippo_frontend_iter2/pages/logs/logs.js
+++ tiny-hippo_frontend_iter2/pages/logs/logs.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-//logs.js
-const util = require('../../utils/util.js')
-
-Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
-})
-=======
 // pages/logs/logs.js
 Page({
 
@@ -81,4 +64,3 @@ Page({
   
   }
 })
->>>>>>> features/BeAShaper
