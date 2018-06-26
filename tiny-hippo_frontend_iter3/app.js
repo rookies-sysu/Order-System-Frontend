3f4ff8e0c03fb2d5cea7e3950662a561b7f9a207
BeAShaper <13136607660@163.com>
2018-06-26 19:18:02 +0800

    iter3

--- tiny-hippo_frontend_iter3/app.js
+++ tiny-hippo_frontend_iter3/app.js
@@ -1,5 +1,9 @@
 //app.js
 App({
+  globalData: {
+    userInfo: null,
+    openid: 'hello'
+  },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -9,7 +13,34 @@ App({
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // ------ 获取凭证 ------
+        var code = res.code;
+        var that = this;
+        if (code) {
+          console.log('获取用户登录凭证：' + code);
+          // ------ 发送凭证 ------
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wxd1baa9ab3bc029ff&secret=a5fb94cc08bfc7e6618a8f3ce10b0f36&js_code=' + code +'&grant_type=authorization_code',
+            data: {},
+            method: 'POST',
+            header: {
+              'content-type': 'application/json'
+            },
+            
+            success: function (res) {
+              if (res.statusCode == 200) {
+                console.log(res)
+                that.globalData.openid = res.data.openid
+                console.log(that.globalData.openid)
+                wx.setStorageSync('openid', res.data.openid)
+              } else {
+                console.log(res.errMsg)
+              }
+            },
+          })
+        } else {
+          console.log('获取用户登录失败：' + res.errMsg);
+        }
       }
     })
     // 获取用户信息
@@ -32,8 +63,5 @@ App({
         }
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
 })
