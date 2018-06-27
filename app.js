2732895993589151c5969e428b0f7a6048051bfc
pcy <908418774@qq.com>
2018-06-27 15:57:21 +0800

    .

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 App({
   globalData: {
     userInfo: null,
-    openid: 'hello'
+    openid: 'hello',
+    cookie:'',
+    tableNum: '',
+    details: ''
   },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    wx.clearStorage();
     // 登录
     wx.login({
       success: res => {
@@ -32,7 +35,7 @@ App({
                 console.log(res)
                 that.globalData.openid = res.data.openid
                 console.log(that.globalData.openid)
-                wx.setStorageSync('openid', res.data.openid)
+                //wx.setStorageSync('openid', res.data.openid)
               } else {
                 console.log(res.errMsg)
               }
@@ -40,26 +43,6 @@ App({
           })
         } else {
           console.log('获取用户登录失败：' + res.errMsg);
-        }
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
         }
       }
     })
