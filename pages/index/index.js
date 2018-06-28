421a2cfcb2a2abbb0993d54991f0586cc99ce6d0
BeAShaper <13136607660@163.com>
2018-06-28 10:26:17 +0800

    change index and type

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,8 @@
 const hSwiper = require('../../component/hSwiper/hSwiper.js');
-//获取应用实例
-const app = getApp()
-
 Page({
-	data: {
-		//swiper插件变量
-		hSwiperVar: {},
+  data: {
+    //swiper插件变量
+    hSwiperVar: {},
     menuSwiperVar: {},
     listData: [],
     foodTypes: 0,
@@ -18,25 +15,29 @@ Page({
     tolMoney: 0,
     foodNumber: 0,
     loading: true,
-    foodTypeList : {},
-    styleValue:{},
-    screenWidth:0,
-    reduceDistance:60,
-    itemWidth:0,
-    startPos:0,
-    touchTime:0,
-    endPos:0,
-    nowTranX:0,
-    nowView:{},
+    foodTypeList: {},
+    styleValue: {},
+    screenWidth: 0,
+    reduceDistance: 60,
+    itemWidth: 0,
+    startPos: 0,
+    touchTime: 0,
+    endPos: 0,
+    nowTranX: 0,
+    nowView: {},
     // 视图过度动画实例
-    swiperAnmiation : {}
-	},
-	onLoad: function () {
+    swiperAnmiation: {}
+  },
+  onLoad: function (options) {
     var that = this;
+    //获取餐桌号
+    var scene = options.tableNum;
+    //存储餐桌号
+    wx.setStorageSync('tableNum', scene);
 
     wx.request({
       /* 自己写的json数据的网址 */
-      url: 'https://www.easy-mock.com/mock/5ab2750509c2ed0d826ee129/example/hippo',
+      url: 'http://111.230.31.38:8080/restaurant/category',
       method: 'GET',
       data: {},
       header: {
@@ -49,12 +50,22 @@ Page({
           listData: res.data,
           loading: false,
         })
+
+        // var listData = res.data
+
+        // for (var index in array) {
+        //   var sexParam = "array[" + index + "].sex"
+        //   that.setData({
+        //     [sexParam]: "nan",
+        //   })
+        // }
+
         that.initialData()
       }
     });
-    
-	},
-	onReady: function() {
+
+  },
+  onReady: function () {
     var swiper = new hSwiper({
       reduceDistance: this.data.reduceDistance,
       varStr: 'hSwiperVar',
@@ -68,31 +79,40 @@ Page({
         'http://wx4.sinaimg.cn/mw690/006fVSiZgy1frempiddekj30hm0bjjsm.jpg',
         'http://wx2.sinaimg.cn/mw690/006fVSiZgy1frempm4glyj30hj0bqq3j.jpg']
     });
-    
+
     var width = wx.getSystemInfoSync().windowWidth;
     this.setData({
-      screenWidth : width,
+      screenWidth: width,
       itemWidth: width - this.data.reduceDistance
     })
     setInterval(function () {
       swiper.nextView();
-    }, 2000);    
+    }, 2000);
 
     setInterval(function () {
       swiper.moveViewTo(0);
-    }, 10000); 
+    }, 10000);
   },
-  initialData : function() {
+  initialData: function () {
     var listData = this.data.listData;
+    var that = this
+    for (var i = 0; i < listData.length; i++) {
+      for (var j = 0; j < listData[i].dish.length; j++) {
+        var numParam = "listData[" + i + "].dish[" + j + "].number"
+        that.setData({
+          [numParam]: 0,
+        })
+      }
+    }
     var count = listData.length;
     for (var i = 0; i < count; i++) {
       this.setData({
-        ["foodTypeList.a"+i]: listData[i].foods.length, 
-        ["styleValue." + i]: 2*(this.data.itemWidth+10),
+        ["foodTypeList.a" + i]: listData[i].dish.length,
+        ["styleValue." + i]: 2 * (this.data.itemWidth + 10),
         ["nowView." + i]: 0
       })
     }
-  }, 
+  },
   onShow: function (options) {
     var that = this
     wx.getStorage({
@@ -124,7 +144,7 @@ Page({
     })
 
   },
-  changeFoodNum:function() {
+  changeFoodNum: function () {
     var that = this
     var loading = that.data.loading
     if (!loading) {
@@ -158,14 +178,14 @@ Page({
       })
     }
   },
-  menuTouchstart : function(e) {
+  menuTouchstart: function (e) {
     this.setData({
       startPos: e.touches[0].clientX,
       touchTime: e.timeStamp
     })
   },
-  menuTouchmove : function(e) {
-    var self = this; 
+  menuTouchmove: function (e) {
+    var self = this;
     var id = e.currentTarget.dataset.type;
     this.setData({
       endPos: e.touches[0].clientX
@@ -173,7 +193,7 @@ Page({
     var delta = self.data.endPos - self.data.startPos
     this.menuMovePos(id, delta);
   },
-  menuTouchend : function (e) {
+  menuTouchend: function (e) {
     var self = this;
     var id = e.currentTarget.dataset.type;
     var delta = e.changedTouches[0].clientX - self.data.startPos
@@ -182,11 +202,11 @@ Page({
 
     if (times < 500 && distance > 50) {
       if (!((e.changedTouches[0].clientX - self.data.startPos) > 0)) {
-         this.nextView(id);
+        this.nextView(id);
       } else {
-         this.preView(id);
+        this.preView(id);
       }
-    } else if(distance > 0) {
+    } else if (distance > 0) {
       this.menuMovePos(id, delta);
       var nowtranX = this.data.nowTranX
       var endpos = self.data.endPos
@@ -198,7 +218,7 @@ Page({
       self.moveViewTo(id, self.getNowView(id));
     }
   },
-  menuMovePos : function (id, x) {
+  menuMovePos: function (id, x) {
     var tempPos = this.data.nowTranX + x,
       minPos = - this.data.itemWidth - 20,
       maxPos = 20;
@@ -211,7 +231,7 @@ Page({
     }
     this.updateMoveAnimation(id, tempPos);
   },
-  updateMoveAnimation : function(id, x) {
+  updateMoveAnimation: function (id, x) {
     var animation = wx.createAnimation({
       transformOrigin: '50% 50%',
       duration: 0,
@@ -223,7 +243,7 @@ Page({
       ["swiperAnmiation." + id]: animation.export()
     })
   },
-  getNowView : function(id) {
+  getNowView: function (id) {
     var indexView = this.data.nowView[id];
     if (this.data.nowTranX > 0) {
       return 0;
@@ -231,7 +251,7 @@ Page({
     indexView = indexView > 0 ? indexView : 0;
     return indexView;
   },
-  nextView : function(id) {
+  nextView: function (id) {
     var index = parseInt(this.data.nowView[id]) + 1;
     index = index > 1 ? 1 : index;
     this.setData({
@@ -240,7 +260,7 @@ Page({
     this.moveViewTo(id, index);
     return index;
   },
-  preView : function(id) {
+  preView: function (id) {
     var index = this.data.nowView[id] - 1;
     index = index < 0 ? 0 : index;
     this.setData({
@@ -249,9 +269,9 @@ Page({
     this.moveViewTo(id, index);
     return index;
   },
-  moveViewTo: function(id, viewIndex) {
+  moveViewTo: function (id, viewIndex) {
     this.setData({
-      nowTranX: viewIndex == 1 ? -(this.data.itemWidth) * viewIndex-5 : (this.data.itemWidth) * viewIndex,
+      nowTranX: viewIndex == 1 ? -(this.data.itemWidth) * viewIndex - 5 : (this.data.itemWidth) * viewIndex,
       ["nowView." + id]: viewIndex
     })
     this.updateViewAnimation(id, this.data.nowTranX);
@@ -269,113 +289,20 @@ Page({
       ["swiperAnmiation." + id]: animation.export()
     })
   },
-  
-  //生成个人订单
-  createOrderJson: function () {
-    var that = this;
-    var cartList = that.data.cartList;
-    var dishList = [];
-
-    for (var i = 0; i < cartList.length; i++) {
-      var item = {
-        "DishID": 0 /*cartList[i].DishID*/,
-        "CategoryID": cartList[i].type/*cartList[i].CategoryID*/,
-        "name": cartList[i].name,
-        "price": cartList[i].price,
-        "number": cartList[i].number,
-        "sum": cartList[i].sum,
-        "orderedNumber": cartList[i].orderedNumber
-      }
-      dishList.push(item);
-    };
-
-    var orderObject = {
-      'items': {
-        "table": parseInt(app.globalData.tableNum),
-        "dish": dishList,
-        "requirement": [
-          {
-            "description": app.globalData.details
-          }
-        ],
-        "price": that.data.tolMoney,
-        "customerId": app.globalData.openid
-      }
-    };
-    return orderObject;
-  },
-
-  //提交个人订单
-  postOrder: function () {
-    var that = this;
-
-    if (app.globalData.openid == '') {
-      console.log('empty customerID')
-      return;
-    }
-    //重新record
-    wx.request({
-      url: 'http://111.230.31.38:8080/restaurant/customer/record',
-      data: {
-        'table': app.globalData.tableNum,
-        'CustomerID': app.globalData.openid,
-        'name': app.globalData.userInfo.nickName,
-        'image': app.globalData.userInfo.avatarUrl
-      },
-      method: "POST",
-      header: {
-        'content-type': 'application/json',
-        'Cookie': app.globalData.cookie
-      },
-      complete: function (res) {
-        console.log(res)
-        if (res.statusCode != 200) {
-          console.log('error ' + res.errMsg)
-          return;
-        }
-      }
-    })
-
-    //发送个人订单
-    wx.request({
-      url: 'http://111.230.31.38:8080/restaurant/customer/edit',
-      data: that.createOrderJson(),
-      method: "POST",
-      header: {
-        'content-type': 'application/json',
-        'Cookie': app.globalData.cookie
-      },
-      complete: function (res) {
-        if (res.statusCode != 200) {
-          console.log('error ' + res.errMsg)
-          return;
-        }
-        console.log(res.data)
-      }
-    })
-  },
-  
   /**
    * 添加到购物车
    */
-
   addToCart: function (e) {
-    var type = e.currentTarget.dataset.type;
+    var _type = e.currentTarget.dataset.type;
     var index = e.currentTarget.dataset.index;
-    var that = this;
-    var selected_item = this.data.listData[type].foods[index];
+    var selected_item = this.data.listData[_type].dish[index];
     var listData = this.data.listData;
-    var number = selected_item.number + 1;
-    for (var i = 0; i < listData.length; i++) {
-      for (var j = 0; j < listData[i].foods.length; j++) {
-        if (listData[i].foods[j].name == selected_item.name) {
-          listData[i].foods[j].number = number;
-        }
-      }
-    }
+
+    var _number = selected_item.number + 1;
+    listData[_type].dish[index].number = _number;
 
     this.setData({
-      currentType: type,
+      currentType: _type,
       currentIndex: index,
       listData: listData
     });
@@ -394,16 +321,16 @@ Page({
     }
     if (unselected) {
       var addItem = {
-        "name": a.listData[a.currentType].foods[a.currentIndex].name,
-        "price": a.listData[a.currentType].foods[a.currentIndex].price,
-        "number": number,
-        "sum": a.listData[a.currentType].foods[a.currentIndex].price,
-        "type": type,
+        "name": a.listData[a.currentType].dish[a.currentIndex].name,
+        "price": a.listData[a.currentType].dish[a.currentIndex].price,
+        "number": _number,
+        "sum": a.listData[a.currentType].dish[a.currentIndex].price,
+        "type": _type,
         "index": index,
         //该菜品已下单数量
         "orderedNumber": 0
       }
-      sum = a.tolMoney + a.listData[a.currentType].foods[a.currentIndex].price;
+      sum = a.tolMoney + a.listData[a.currentType].dish[a.currentIndex].price;
       cartList.push(addItem);
     }
     this.setData({
@@ -411,21 +338,18 @@ Page({
       tolMoney: sum,
       foodNumber: a.foodNumber + 1
     });
-
-    that.postOrder();
   },
   /*从图片处减少商品*/
   minusFromMenu: function (e) {
-    var type = e.currentTarget.dataset.type;
+    var _type = e.currentTarget.dataset.type;
     var index = e.currentTarget.dataset.index;
-    var that = this;
 
     var cartList = this.data.cartList;
     var sum;
 
     for (var i = 0; i < cartList.length; i++) {
-      if (cartList[i].name == this.data.listData[type].foods[index].name) {
-        
+      if (cartList[i].name == this.data.listData[_type].dish[index].name) {
+
         //选中的菜品的数量不能小于已经下单的该菜品的数量
         if (cartList[i].orderedNumber != 0 && cartList[i].orderedNumber == cartList[i].number) {
           wx.showModal({
@@ -442,27 +366,27 @@ Page({
       }
     }
 
-    var selected_item = this.data.listData[type].foods[index];
+    var selected_item = this.data.listData[_type].dish[index];
 
     var listData = this.data.listData;
-    var number = selected_item.number - 1;
-    for (var i = 0; i < listData.length; i++) {
-      for (var j = 0; j < listData[i].foods.length; j++) {
-        if (listData[i].foods[j].name == selected_item.name) {
-          listData[i].foods[j].number = number;
-        }
-      }
-    }
+    var _number = selected_item.number - 1;
+    listData[_type].dish[index].number = _number;
+
+    // for (var i = 0; i < listData.length; i++) {
+    //   for (var j = 0; j < listData[i].foods.length; j++) {
+    //     if (listData[i].foods[j].name == selected_item.name) {
+    //       listData[i].foods[j].number = number;
+    //     }
+    //   }
+    // }
 
     this.setData({
-        listData: listData,
-        cartList: cartList,
-        tolMoney: sum,
-        foodNumber: this.data.foodNumber - 1
+      listData: listData,
+      cartList: cartList,
+      tolMoney: sum,
+      foodNumber: this.data.foodNumber - 1
     });
 
-    that.postOrder();
-    
   },
   onHide: function () {
     if (this.data.tolMonney != 0) {
